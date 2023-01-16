689abc172957f65c38027a30250a7547581b021c
m-ghiringhelli <mghiring@gmail.com>
2023-01-16 12:47:27 -0800

    add state for form component

--- src/components/AuthForm/AuthForm.js
+++ src/components/AuthForm/AuthForm.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './AuthForm.css';
 
 export default function AuthForm() {
+  const [userInfo, setUserInfo] = useState({
+    email: '',
+    password: ''
+  });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <form className='auth-form'>
+    <form 
+      className='auth-form'
+      onSubmit={handleSubmit}
+    >
       <label>
         email:
         <input 
           name='email' 
           type='email' 
-          placeholder='email' />
+          placeholder='email'
+          onChange={(e) => setUserInfo({
+            ...userInfo,
+            email: e.target.value
+          })}
+        />
       </label>
       <label>
         password:
@@ -17,6 +33,10 @@ export default function AuthForm() {
           name='password'
           type='password' 
           placeholder='password'
+          onChange={(e) => setUserInfo({
+            ...userInfo,
+            password: e.target.value
+          })}
         />
       </label>
       <button>submit</button>

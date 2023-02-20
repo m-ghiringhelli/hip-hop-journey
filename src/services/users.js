5a6772f755e693ad5d246133d42355784e7fcda4
m-ghiringhelli <mghiring@gmail.com>
2023-02-20 13:09:13 -0800

    enable signin to access db / receive and store cookie

--- src/services/users.js
+++ src/services/users.js
@@ -1,6 +1,4 @@
 export const signUp = async ({ email, password }) => {
-  console.log(`${process.env.REACT_APP_API_URL}`);
-  console.log('in signup', email, password);
   const resp = await fetch(`${process.env.REACT_APP_API_URL}/api/v1/users`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
@@ -14,6 +12,16 @@ export const signUp = async ({ email, password }) => {
   return resp.json();
 };
 
-export function signIn(user) {
-  console.log('signin', user);
-}
+export const signIn = async ({ email, password }) => {
+  const resp = await fetch(`${process.env.REACT_APP_API_URL}/api/v1/users/sessions`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    credentials: 'include',
+    mode: 'cors',
+    body: JSON.stringify({ email, password }),
+  });
+
+  if (!resp.ok) throw new Error('trouble signing in');
+
+  return resp.json();
+};

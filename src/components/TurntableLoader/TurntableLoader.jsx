ac16cdc8187f463e156e2292bef22753dc105b4a
m-ghiringhelli <mghiring@gmail.com>
2023-02-23 14:22:54 -0800

    add tone arm to css loader

--- src/components/TurntableLoader/TurntableLoader.jsx
+++ src/components/TurntableLoader/TurntableLoader.jsx
@@ -8,9 +8,19 @@ export default function TurntableLoader() {
         <div className='turntable-ring first'>
           <div className='turntable-ring second'>
             <div className='turntable-label'>
+              <p>loading...
+              </p>
               <div className='turntable-point'></div>
             </div>
           </div>
+        </div>
+      </div>
+      <div className='turntable-tone-arm'>
+        <div className='turntable-tone-arm-weight'></div>
+        <div className='turntable-tone-arm-arm'></div>
+        <div className='turntable-tone-arm-cartridge'>
+          <div className='cartridge-arm'></div>
+          <div className='cartridge-cartridge'></div>
         </div>
       </div>
     </div>
